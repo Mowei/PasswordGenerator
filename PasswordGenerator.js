a46e714adc41e32765579fb018c6c0cb18c9c7f3
Moti <motitseng@utron.com.tw>
2018-04-01 22:30:08 +0800

    add js

---
+++ PasswordGenerator.js
@@ -0,0 +1,1 @@
+(function(len){let arr=['\\','/','|','!','i','I','l','1','J','[',']','{',']'];let newPass='';for(let i=0;i<len;i++){newPass+=arr[Math.floor(Math.random()*arr.length)]};console.log(newPass)})(10)
